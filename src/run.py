08c8d7981470c0d8328da8b602319d945c5d6d25
ryqdev <ykshang4119@gmail.com>
2024-05-02 12:27:29 +0800

    add argparse

--- src/run.py
+++ src/run.py
@@ -1,1 +1,8 @@
-print(23)
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='tttest')
+    parser.add_argument('--method', type=str, help='the http method type (get | post)',
+                        choices=["get", "post"], default='get')
+    
+    args = parser.parse_args()
